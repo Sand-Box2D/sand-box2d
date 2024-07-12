0b229d708e3b3b2fb69fc6c3932d756203057c0a
hammerill <kyrylo@hammerill.com>
2024-07-12 16:48:32 +0200

    Add some comments

--- src/models/physians/physian.hpp
+++ src/models/physians/physian.hpp
@@ -1,1 +1,9 @@
 #pragma once
+
+/**
+ * @brief Physian is an abstract base physics object in the Sand-Box2D level.
+ * It could be a box, a platform, etc.
+ * 
+ * You could render all the objects by calling `render()` method on all the Physians.
+ * Concrete objects would pick up their own render method implementations.
+ */
--- src/renderer/renderer.hpp
+++ src/renderer/renderer.hpp
@@ -1,1 +1,11 @@
 #pragma once
+
+/**
+ * @brief Renderer class implements basic functions of a renderer environment.
+ * It initializes the window and its renderer, lets you get/set resolution and,
+ * most importantly, provides you with the basic rendering functions
+ * such as "render this texture there with this angle".
+ * 
+ * It could be implemented differently on different platforms.
+ * For now, only SDL2 renderer is implemented.
+ */
