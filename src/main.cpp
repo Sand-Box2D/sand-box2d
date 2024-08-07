a13341d936f9a8a72e668e85782d8d257d4f0210
hammerill <kyrylo@hammerill.com>
2024-08-08 01:02:17 +0200

    Update test main

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@ int main()
 {
     auto renderer = Renderer();
 
-    renderer.init({1, RR_MODE_WINDOW, 1920/2, 1080/2});
+    renderer.init();
 
-    for (int i = 0; i < 2000; i++)  // A cheap way to close the game
+    for (int i = 0; i < 1000; i++)  // A cheap way to close the game
     {
         renderer.clearScreen({0, 0, 0xFF});
         renderer.render();
